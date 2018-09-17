5a8e613ed6d306bbd0d58eabf639e7217297ef01
Gorik <43270381+Gorik1@users.noreply.github.com>
2018-09-18 00:28:17 +0200

    Added Neural Network python script
    Simple Neural Network python script added

---
+++ NN.py
@@ -0,0 +1,26 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras import optimizers
+from keras.utils import plot_model
+import NNUtility as nnu
+import numpy as np
+import math
+
+train_x, train_y = nnu.load_set('./summary_train.par')
+validate_x, validate_y = nnu.load_set('./summary_validate.txt')
+
+model = Sequential()
+model.add(Dense(14, input_dim=14, activation='sigmoid'))
+model.add(Dense(7, activation='relu'))
+model.add(Dense(2, activation='relu'))
+
+sgd = optimizers.SGD(lr=0.01, momentum=0.01, decay=0.001)
+
+model.compile(optimizer=sgd, loss='mean_squared_error', metrics=['accuracy'])
+model.fit(x=train_x, y=train_y, batch_size=32, epochs=100, verbose=2,
+          callbacks=None, validation_split=0,
+          validation_data=(validate_x, validate_y), shuffle=False,
+          class_weight=None, sample_weight=None, initial_epoch=0,
+          steps_per_epoch=None, validation_steps=None)
+
+plot_model(model, to_file='.\result.png')
---
+++ NNUtility.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def load_set(path):
+    data = np.loadtxt(path)
+    x_data = data[:, :-2]
+    y_data = data[:, -2:]
+    return x_data, y_data
+
+def norm_var(data):
+    for column in range(data.shape[1]):
+        data[column] = data[column] / np.var(data[column])
+    return data
+
+def custom_loss(y_true, y_pred):
+    ret_up = np.square(y_true[0] + y_true[1] - y_pred[0])
+    ret_low = np.square(y_true[0] - y_true[1] - y_pred[1])
+    return ret_up, ret_low
