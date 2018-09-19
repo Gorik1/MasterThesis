ef282eba253bab33447fdc3dd3e26b323cd93451
Gorik <43270381+Gorik1@users.noreply.github.com>
2018-09-20 00:41:00 +0200

    Worked on NN

--- NN.py
+++ NN.py
@@ -6,7 +6,7 @@ import NNUtility as nnu
 import numpy as np
 import math
 
-train_x, train_y = nnu.load_set('./summary_train.par')
+train_x, train_y = nnu.load_set('./summary_train.txt')
 validate_x, validate_y = nnu.load_set('./summary_validate.txt')
 
 model = Sequential()
@@ -17,7 +17,7 @@ model.add(Dense(2, activation='relu'))
 sgd = optimizers.SGD(lr=0.01, momentum=0.01, decay=0.001)
 
 model.compile(optimizer=sgd, loss='mean_squared_error', metrics=['accuracy'])
-model.fit(x=train_x, y=train_y, batch_size=32, epochs=100, verbose=2,
+model.fit(x=train_x, y=train_y, batch_size=32, epochs=100, verbose=1,
           callbacks=None, validation_split=0,
           validation_data=(validate_x, validate_y), shuffle=False,
           class_weight=None, sample_weight=None, initial_epoch=0,
